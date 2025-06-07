8e7d622ac7504a704333c6b160187b1cb6fcac24
Tiffany Hurkman <ncgirlbrodey@yahoo.com>
2025-06-07 15:03:58 +0000

    chapter 2

---
+++ _02_03b/Ticket.java
@@ -0,0 +1,16 @@
+package _02_03b;
+
+/**
+ * 2. Give the class an empty constructor
+ * 3. Create 3 field variables in the class:
+ * - A field of type String called destination
+ * - A field called price of type double
+ * - A field called isReturn of type boolean
+ */
+public class Ticket {
+  public Ticket(){
+  String destination;
+  double price;
+  boolean isReturn;
+  }
+}
